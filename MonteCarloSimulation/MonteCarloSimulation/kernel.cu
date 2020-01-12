13c94aae2c43924e063a827d5928c7396b471022
Vlad-Mihai Savin <savin.vladmihai@gmail.com>
2020-01-12 22:55:40 +0200

    generated random samples on device instead of host

--- MonteCarloSimulation/MonteCarloSimulation/kernel.cu
+++ MonteCarloSimulation/MonteCarloSimulation/kernel.cu
@@ -6,36 +6,36 @@
 #include <stdlib.h>
 #include <time.h>
 #include <ctime>
+#include <cuda.h>
+#include <curand.h>
+#include <curand_kernel.h>
 
-#define NUM_BLOCKS 1024
-#define NUM_THREADS_PER_BLOCK 1024
-#define NUM_SAMPLES 1e6
-
-float* generateSamples(int size) {
-    float* array = new float[size];
-    srand(time(NULL));
-
-    for (int i = 0; i < size; ++i) {
-        array[i] = (float) rand() / RAND_MAX;
-    }
-    return array;
-}
+#define NUM_BLOCKS 2048
+#define NUM_THREADS_PER_BLOCK 32
+#define NUM_SAMPLES_PER_THREAD 1e3 * 2
+#define KERNEL_ITERATIONS 1e1
 
 float getPiValue(int numSamplesInCircle) {
-    return 4.0 * (float) numSamplesInCircle / NUM_SAMPLES;
+    return 4.0 * (float) numSamplesInCircle / (NUM_SAMPLES_PER_THREAD * NUM_THREADS_PER_BLOCK * NUM_BLOCKS * KERNEL_ITERATIONS);
 }
 
-cudaError_t countSamplesInCircleWithCuda(float* host_randX, float* host_randY, int* host_sampleCountPerBlock);
+cudaError_t countSamplesInCircleWithCuda(int iterationNo, int* host_sampleCountPerBlock);
 
-__global__ void countSamplesInCircleKernel(float* device_randX, float* device_randY, int* device_sampleCountPerBlock)
+__global__ void countSamplesInCircleKernel(int* device_sampleCountPerBlock, curandState* states)
 {
     __shared__ int sampleCountPerThreads[NUM_THREADS_PER_BLOCK];
 
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    float device_x, device_y;
+
+    curand_init(clock64(), idx, 0, &states[idx]);
+
     int samplesInCircle = 0;
 
-    for (int i = idx; i < NUM_SAMPLES; i += blockDim.x * NUM_BLOCKS) {
-        if (device_randX[i] * device_randX[i] + device_randY[i] * device_randY[i] <= 1.0f) {
+    for (int i = 0; i < NUM_SAMPLES_PER_THREAD; ++i) {
+        device_x = curand_uniform(&states[idx]); // Random x position in [0,1]
+        device_y = curand_uniform(&states[idx]); // Random y position in [0,1]
+        if (device_x * device_x + device_y * device_y <= 1.0f) {
             samplesInCircle++;
         }
     }
@@ -54,32 +54,29 @@ __global__ void countSamplesInCircleKernel(float* device_randX, float* device_ra
 int main()
 {
     std::clock_t c_start = std::clock();
-    printf("Generating host X samples...\n");
-    float* host_randX = generateSamples(NUM_SAMPLES);
-
-    printf("Generating host Y samples...\n");
-    float* host_randY = generateSamples(NUM_SAMPLES);
-    std::clock_t c_end = std::clock();
-
-    long double time_elapsed_ms = 1000.0 * (c_end - c_start) / CLOCKS_PER_SEC;
-    printf("Generated vectors in: %lf ms\n", time_elapsed_ms);
 
     int* host_sampleCountPerBlock = new int[NUM_BLOCKS];
-
-    // Call kernel    
-    cudaError_t cudaStatus = countSamplesInCircleWithCuda(host_randX, host_randY, host_sampleCountPerBlock);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "countSamplesInCircleWithCuda failed!");
-        return 1;
-    }
-
     int numSamplesInCircle = 0;
-    for (int i = 0; i < NUM_BLOCKS; ++i) {
-        numSamplesInCircle += host_sampleCountPerBlock[i];
+    cudaError_t cudaStatus;
+    int memorySize = int(NUM_SAMPLES_PER_THREAD * NUM_THREADS_PER_BLOCK * NUM_BLOCKS) / 1024 / 1024 * 4;
+
+    for (int i = 0; i < KERNEL_ITERATIONS; ++i) {
+        // Call kernel    
+        cudaStatus = countSamplesInCircleWithCuda(i, host_sampleCountPerBlock);
+        if (cudaStatus != cudaSuccess) {
+            fprintf(stderr, "countSamplesInCircleWithCuda failed!");
+            return 1;
+        }
+
+        for (int i = 0; i < NUM_BLOCKS; ++i) {
+            numSamplesInCircle += host_sampleCountPerBlock[i];
+        }
+
+        printf("Device memory used: %dMb\n", memorySize);
     }
+   
 
     float piValue = getPiValue(numSamplesInCircle);
-    printf("pi = %f\n", piValue);
 
     // cudaDeviceReset must be called before exiting in order for profiling and
     // tracing tools such as Nsight and Visual Profiler to show complete traces.
@@ -89,11 +86,17 @@ int main()
         return 1;
     }
 
+    std::clock_t c_end = std::clock();
+    long double time_elapsed_ms = 1000.0 * (c_end - c_start) / CLOCKS_PER_SEC;
+    printf("Total processing time: %lf ms\n", time_elapsed_ms);
+    printf("Number of samples: %d\n", int(NUM_SAMPLES_PER_THREAD * NUM_THREADS_PER_BLOCK * NUM_BLOCKS * KERNEL_ITERATIONS));
+    printf("pi = %f\n", piValue);
+
     return 0;
 }
 
 // Helper function for using CUDA to add vectors in parallel.
-cudaError_t countSamplesInCircleWithCuda(float* host_randX, float* host_randY, int* host_sampleCountPerBlock)
+cudaError_t countSamplesInCircleWithCuda(int iterationNo, int* host_sampleCountPerBlock)
 {
     cudaError_t cudaStatus;
     cudaEvent_t start1, stop1, start2, stop2, start3, stop3;
@@ -111,69 +114,27 @@ cudaError_t countSamplesInCircleWithCuda(float* host_randX, float* host_randY, i
         goto Error;
     }
 
-    const size_t SAMPLE_SIZE = NUM_SAMPLES * sizeof(float);
-    float* device_randX;
-    float* device_randY;
-
-    printf("Device malloc randX...\n");
-    cudaEventRecord(start1, 0);
-    cudaStatus = cudaMalloc(&device_randX, SAMPLE_SIZE);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed! (device_randX)\n");
-        goto Error;
-    }
-
-    printf("Device malloc randY...\n");
-    cudaStatus = cudaMalloc(&device_randY, SAMPLE_SIZE);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed! (device_randY)\n");
-        goto Error;
-    }
-
-    // Copy input vectors from host memory to GPU buffers.
-    printf("Copying randX from host to device...\n");
-    cudaStatus = cudaMemcpy(device_randX, host_randX, SAMPLE_SIZE, cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed! (device_randX)\n");
-        goto Error;
-    }
-
-    printf("Copying randY from host to device...\n");
-    cudaStatus = cudaMemcpy(device_randY, host_randY, SAMPLE_SIZE, cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed! (device_randY)\n");
-        goto Error;
-    }
-    cudaEventRecord(stop1, 0);
-    cudaEventSynchronize(stop1);
-    float elapsedTime;
-    cudaEventElapsedTime(&elapsedTime, start1, stop1);
-    printf("Malloc and copy from --proc-- to --graphics card-- time: %f\n", elapsedTime);
-
     const size_t COUNT_SIZE = NUM_BLOCKS * sizeof(int);
+    float elapsedTime;
 
     cudaEventRecord(start2, 0);
-    printf("Device malloc sampleCountPerBlock...\n");
+    printf("\nDevice malloc sampleCountPerBlock...\n");
     int* device_sampleCountPerBlock;
     cudaStatus = cudaMalloc(&device_sampleCountPerBlock, COUNT_SIZE);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMalloc failed! (device_sampleCountPerBlock)\n");
         goto Error;
     }
-
+    curandState* devStates;
+    cudaMalloc((void**)&devStates, NUM_BLOCKS * NUM_THREADS_PER_BLOCK * sizeof(curandState));
     // Launch a kernel on the GPU with one thread for each element.
-    printf("Launching kernel...\n");
-    countSamplesInCircleKernel<<<NUM_BLOCKS, NUM_THREADS_PER_BLOCK>>>(device_randX, device_randY, device_sampleCountPerBlock);
-
-    cudaEventRecord(stop2, 0);
-    cudaEventSynchronize(stop2);
-    cudaEventElapsedTime(&elapsedTime, start2, stop2);
-    printf("Kernel execution time: %f\n", elapsedTime);
+    printf("Launching kernel (%d)...\n", iterationNo);
+    countSamplesInCircleKernel<<<NUM_BLOCKS, NUM_THREADS_PER_BLOCK>>>(device_sampleCountPerBlock, devStates);
 
     // Check for any errors launching the kernel
     cudaStatus = cudaGetLastError();
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        fprintf(stderr, "kernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
         goto Error;
     }
     
@@ -181,10 +142,15 @@ cudaError_t countSamplesInCircleWithCuda(float* host_randX, float* host_randY, i
     // any errors encountered during the launch.
     cudaStatus = cudaDeviceSynchronize();
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching kernel!\n", cudaStatus);
         goto Error;
     }
-    
+
+    cudaEventRecord(stop2, 0);
+    cudaEventSynchronize(stop2);
+    cudaEventElapsedTime(&elapsedTime, start2, stop2);
+    printf("Kernel execution time: %f\n", elapsedTime);
+
     cudaEventRecord(start3, 0);
     printf("Copying sampleCountPerBlock from device to host...\n");
     cudaStatus = cudaMemcpy(host_sampleCountPerBlock, device_sampleCountPerBlock, COUNT_SIZE, cudaMemcpyDeviceToHost);
@@ -198,8 +164,6 @@ cudaError_t countSamplesInCircleWithCuda(float* host_randX, float* host_randY, i
     printf("Copy from --graphics card-- to --proc-- time: %f\n", elapsedTime);
 
 Error:
-    cudaFree(device_randX);
-    cudaFree(device_randY);
     cudaFree(device_sampleCountPerBlock);
     
     return cudaStatus;
